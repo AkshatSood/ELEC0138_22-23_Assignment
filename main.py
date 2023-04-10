27c8ec3cb5838ec1a426937931d7549cc7feaf34
Akshat Sood <soodakshat30@gmail.com>
2023-04-10 10:50:02 +0100

    Add module to download dataset

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 """Main Module"""
 
 from modules.transformation import AudioTransformer
-
+from modules.dataset import Dataset
 
 # Create objects for classes
+# dataset = 
 transformer = AudioTransformer()
+dataset = Dataset()
+
+print('##############################################')
+print('\tDOWNLOAD DATASET')
+print('##############################################')
+dataset.download_data()
 
 print('##############################################')
 print('\tTRANSFORM AUDIO FILES')
 print('##############################################')
-transformer.transform()
+# transformer.transform()
--- modules/dataset.py
+++ modules/dataset.py
@@ -1,6 +1,8 @@
+"""Dataset Module"""
+
+import kaggle
 
 from constants import DATASET_DIR
-
 from modules.utility import Utility
 
 class Dataset:
@@ -9,10 +11,16 @@ class Dataset:
         """Default constructor
         """
         self.utility = Utility()
+        self.dataset_url = 'https://www.kaggle.com/datasets/uwrfkaggler/ravdess-emotional-song-audio/download?datasetVersionNumber=1'
 
+        kaggle.api.authenticate()
+        
         # Check if the dataset directory exists. If not, then create it
         self.utility.check_and_create_dir(DATASET_DIR)
 
     def download_data(self):
-        pass
+        # with urlopen(self.dataset_url) as zipresp:
+        #     with ZipFile(BytesIO(zipresp.read())) as zfile:
+        #         zfile.extractall(DATASET_DIR)
+        kaggle.api.dataset_download_files('RAVDESS Emotional song audio', DATASET_DIR, unzip=True)
 
