90f06312bcd5534a20e60c1a6ddc8547e3de93f0
Akshat Sood <soodakshat30@gmail.com>
2023-04-20 18:14:24 +0100

    Remove unused files

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 """Main Module"""
 
 from modules.transformation import AudioTransformer
-from modules.dataset import Dataset
 
-# Create objects for classes
-# dataset = 
 transformer = AudioTransformer()
-# dataset = Dataset()
-
-# print('##############################################')
-# print('\tDOWNLOAD DATASET')
-# print('##############################################')
-# dataset.download_data()
 
 print('##############################################')
 print('\tTRANSFORM AUDIO FILES')
